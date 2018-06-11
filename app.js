a00082c4ae55b62a6c61eaa0707e225d6c25c616
Alexandr Poliakov <edbutcher200@gmail.com>
2018-06-11 22:59:16 +0300

    Express sessions final part

--- app.js
+++ app.js
@@ -45,51 +45,35 @@ app.use(session({
     store: new FileStore()
 }));
 
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth (req, res, next) {
     console.log(req.session);
 
-    if (!req.session.user) {
-        let authHeader = req.headers.authorization;
-
-        if (!authHeader) {
-            let err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-        
-        let auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-        let user = auth[0];
-        let pass = auth[1];
-
-        if (user == 'admin' && pass == 'password') {
-            req.session.user = 'admin';
-            return next(); // authorized
-        } else {
-            let err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
+    if(!req.session.user) {
+        let err = new Error('You are not authenticated!');
+        err.status = 403;
+        return next(err);
     }
     else {
-        if (req.session.user === 'admin') {
-            return next();
+        if (req.session.user === 'authenticated') {
+            next();
         }
         else {
             let err = new Error('You are not authenticated!');
-            err.status = 401;
+            err.status = 403;
             return next(err);
         }
     }
-}
+};
+
 
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
