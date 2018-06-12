618667d555f4c3e6d988a7e27178ea87ae441e52
Alexandr Poliakov <edbutcher200@gmail.com>
2018-06-12 22:44:17 +0300

    all updates

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url, {
-  useMongoClient: true
+    useMongoClient: true
 });
 
 connect.then((db) => {
-  console.log('Connected correctly to server');
+    console.log('Connected correctly to server');
 }, (err) => { console.log(err); });
 
 var app = express();
@@ -47,6 +47,7 @@ app.use(session({
     store: new FileStore()
 }));
 
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -55,20 +56,18 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-
     console.log(req.user);
 
-    if(!req.user) {
-        let err = new Error('You are not authenticated!');
+    if (!req.user) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
         err.status = 401;
-        return next(err);
+        next(err);
     }
     else {
         next();
-
-
     }
-};
+}
 
 app.use(auth);
 
@@ -80,18 +79,18 @@ app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
